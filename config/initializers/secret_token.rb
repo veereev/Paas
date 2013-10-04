e8724a6e402a30a1b97527d91c7631439a92debf
VEERASEKHAR ADDEPALLI <veerasekhar87@gmail.com>
2013-10-04 15:41:51 -0700

    Application Base Stack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paas::Application.config.secret_token = '68ef0fdef03146eb51c7744b1bd336994c57f83ab93ea4c960c7046c5c988b6e8843efef8438f1b80564287c19ac501b74dd36c534559db899430860e3f5afe8'
